6301e38680d2d819799df75be216c904f9635e46
Daniel Hrehor <dan.hrehor@gmail.com>
2026-02-07 23:18:17 +0100

    modified css, added html elements, modified scroll fade animation

--- js/ScrollAnimation.js
+++ js/ScrollAnimation.js
@@ -1,16 +1,18 @@
 // https://www.youtube.com/watch?v=adqwnu3gs2k
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('in-view');
-        }
-        if (!entry.isIntersecting && entry.target.classList.contains('in-view')) {
-            entry.target.classList.remove('in-view');
-        }
+window.addEventListener('features-created', () => {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('in-view');
+            }
+            if (!entry.isIntersecting && entry.target.classList.contains('in-view')) {
+                entry.target.classList.remove('in-view');
+            }
+        });
+    }, {
+        threshold: 0.6
     });
-}, {
-    threshold: 0.4
-});
 
-const hiddenElements = document.querySelectorAll('.feature');
-hiddenElements.forEach((el) => observer.observe(el));
+    const hiddenElements = document.querySelectorAll('.feature');
+    hiddenElements.forEach((el) => observer.observe(el));
+});
