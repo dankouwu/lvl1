b0f55a5284f69c1a6dcad807f28b2c6f97088c35
Daniel Hrehor <dan.hrehor@gmail.com>
2026-02-10 11:26:55 +0100

    redesigned the footer, fixed footer rendering issues

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -2,6 +2,7 @@ class Footer extends HTMLElement {
     async connectedCallback() {
         const response = await fetch('./components/Footer/footer.html');
         this.innerHTML = await response.text();
+        lucide.createIcons();
     }
 }
 
